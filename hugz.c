03d1b9333e752eeaabaefb8159104f976d5bdd09
Nicolas Lorin <androw95220@gmail.com>
2011-05-23 22:03:55 +0200

    Little fixed

--- hugz.c
+++ hugz.c
@@ -11,8 +11,8 @@ int pathTest (Noeud* teteliste, dalle grid[10][10]) {
         fullPath = 1;
         for (i=0;i<10;i++) {
             for (j=0;j<10;j++) {//...pour chaque case...
-                if (grid[i][j].joueur != l->grille[i][j].joueur || grid[i][j].pion->type != l->grille[i][j].pion->type) {
-                    fullPath = 0; //...il y a une diffÃ©rence donc pas de situation d'egalite...
+                if (grid[i][j].joueur != l->grille[i][j].joueur || grid[i][j].pion != l->grille[i][j].pion) {
+                    fullPath = 0; //...il y a une différence donc pas de situation d'egalite...
                 }
                 else  {
                     fullPath = fullPath && 1;//...il n'y a pas de difference pour cette case, on continue a verifier
@@ -24,24 +24,25 @@ int pathTest (Noeud* teteliste, dalle grid[10][10]) {
             if (l->acc == 3) {//cas ou la partie est nulle (meme situation 3fois)
                 return 1;
             }
+            else {return 0;}
         }
         else { // on verifie la situation enregistree plus recement
-		if (l->suivant != NULL) {
-            		l = l->suivant;
-		} else {
-			Noeud* p = (Noeud*)malloc(sizeof(Noeud)); //crÃ©ation du nouveau noeud pour la grille
-        		p->acc=1;//situation rencontrÃ©e deja une fois
-        		for (i=0;i<10;i++) {
-            			for (j=0;j<10;j++) {//copie de la grille
-                			p->grille[i][j].pion->type = grid[i][j].pion->type;
-                			p->grille[i][j].joueur = grid[i][j].joueur;
-            			}
-        		}
-        		p->suivant=NULL;//ajoutfin
-        		l->suivant=p;
-        		return 0;
-        	}
-    	}
-	}
-    return 0; 
+        if (l->suivant != NULL) {
+                    l = l->suivant;
+        } else {
+            Noeud* p = (Noeud*)malloc(sizeof(Noeud)); //création du nouveau noeud pour la grille
+                p->acc=0;//situation rencontrée deja une fois
+                for (i=0;i<10;i++) {
+                        for (j=0;j<10;j++) {//copie de la grille
+                            p->grille[i][j].pion = grid[i][j].pion;
+                            p->grille[i][j].joueur = grid[i][j].joueur;
+                        }
+                }
+                p->suivant=NULL;//ajoutfin
+                l->suivant=p;
+                return 0;
+            }
+        }
+    }
+    return 0;
 }
