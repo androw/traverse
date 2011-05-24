c416773e45fe11b0c5220427841cad98cd354a9b
Nicolas Lorin <androw95220@gmail.com>
2011-05-24 17:19:03 +0200

    New files

---
+++ arb.h
@@ -0,0 +1,17 @@
+#ifndef ARB_H
+#define ARB_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "dalle.h"
+
+typedef struct Noeud {
+	dalle grille[10][10];
+	int acc;
+	struct Noeud* suivant;
+} Noeud;
+
+int pathTest (Noeud* teteliste, dalle grid[10][10]);
+int condWin(dalle grid[10][10],int player);
+int condWinHM(dalle grid[10][10],int player);
+#endif
---
+++ dalle.h
@@ -0,0 +1,16 @@
+#ifndef DALLE_H
+#define DALLE_H
+
+#include "pion.h"
+
+typedef struct dalle{
+	struct pion* pion;
+	int border;
+	int color;
+	int djoueur;
+	int ajoueur;
+	int joueur;
+	int pass;
+}dalle;
+
+#endif
